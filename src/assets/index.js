b6258e7b7a4db28e25ac013247fa82480e41f9e9
Usman972002 <usmanfaruq21@gmail.com>
2024-11-18 10:56:14 +0530

    Added Navbar and Hero section

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import round from './round.png';
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -28,7 +29,7 @@ import tesla from "./company/tesla.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import tripguide from "./tripguide.png"; 
 
 export {
   logo,
@@ -59,4 +60,5 @@ export {
   carrent,
   jobit,
   tripguide,
+  round,
 };
