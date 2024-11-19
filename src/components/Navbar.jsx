8fd59afe181fdc44ad12dd13529deba964c16e87
Usman972002 <usmanfaruq21@gmail.com>
2024-11-19 13:55:39 +0530

    Added the main link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
-          to="/"
+          to="/3dportfolio"
           className="flex items-center gap-2"
           onClick={() => {
             setActive("");
