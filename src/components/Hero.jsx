23af31797dd44458a6b130fb2d905bb154f09803
Usman972002 <usmanfaruq21@gmail.com>
2024-11-19 15:56:20 +0530

    Added Styling for Scroll

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,14 +28,14 @@ const Hero = () => {
     mt-2 text-white-100
     `}
           >
-            I Develop Full Stack web Applications
+            I Develop Full Stack web Applications 
             <br className="sm:block hidden" />
             using MERN and Java Stack
           </p>
         </div>
       </div>
       <ComputersCanvas />
-      <div className="absolute bottom-4 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
       <a href="#about">
         <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
           <motion.dev animate={{
